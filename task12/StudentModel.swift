49a02f4be5ef66230676534bbb2cfe55cf3375f5
Stacy Grape <grapeovastacy@gmail.com>
2021-06-19 09:52:38 +0300

    Add

---
+++ task12/StudentModel.swift
@@ -0,0 +1,18 @@
+//
+//  StudentModel.swift
+//  task12
+//
+//  Created by Stacy Vinogradova on 19.06.21.
+//
+
+import Foundation
+
+struct Student {
+    var name: String = ""
+    var surname: String = ""
+    var yearOfBirth: String = ""
+    var sex: String = ""
+    var course: String = ""
+    var group: String = ""
+    var studentID: String = ""
+}
---
+++ task12/StudentSubject.swift
@@ -0,0 +1,18 @@
+//
+//  StudentSubject.swift
+//  task12
+//
+//  Created by Stacy Vinogradova on 19.06.21.
+//
+
+import Foundation
+
+
+struct Subject {
+    var examType: String = ""
+    var date: String = ""
+    var subjectName: String = ""
+    var currentRating: String = ""
+    var examRating: String = ""
+    var totalRating: String = ""
+}
